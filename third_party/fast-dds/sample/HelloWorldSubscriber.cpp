4fad53df5b57bdfeba0650d883b7e60ee90aeaa4
KANGWEII <pkangwei@outlook.com>
2025-05-22 00:21:46 +0800

    third_party: fast-dds: sample: update HelloWorld code style

--- third_party/fast-dds/sample/HelloWorldSubscriber.cpp
+++ third_party/fast-dds/sample/HelloWorldSubscriber.cpp
@@ -33,169 +33,136 @@
 
 using namespace eprosima::fastdds::dds;
 
-class HelloWorldSubscriber
-{
-private:
+class HelloWorldSubscriber {
+ private:
+  DomainParticipant* participant_;
 
-    DomainParticipant* participant_;
+  Subscriber* subscriber_;
 
-    Subscriber* subscriber_;
+  DataReader* reader_;
 
-    DataReader* reader_;
+  Topic* topic_;
 
-    Topic* topic_;
+  TypeSupport type_;
 
-    TypeSupport type_;
+  class SubListener : public DataReaderListener {
+   public:
+    SubListener() : samples_(0) {}
 
-    class SubListener : public DataReaderListener
-    {
-    public:
+    ~SubListener() override {}
 
-        SubListener()
-            : samples_(0)
-        {
-        }
-
-        ~SubListener() override
-        {
-        }
-
-        void on_subscription_matched(
-                DataReader*,
-                const SubscriptionMatchedStatus& info) override
-        {
-            if (info.current_count_change == 1)
-            {
-                std::cout << "Subscriber matched." << std::endl;
-            }
-            else if (info.current_count_change == -1)
-            {
-                std::cout << "Subscriber unmatched." << std::endl;
-            }
-            else
-            {
-                std::cout << info.current_count_change
-                          << " is not a valid value for SubscriptionMatchedStatus current count change" << std::endl;
-            }
-        }
-
-        void on_data_available(
-                DataReader* reader) override
-        {
-            SampleInfo info;
-            if (reader->take_next_sample(&hello_, &info) == eprosima::fastdds::dds::RETCODE_OK)
-            {
-                if (info.valid_data)
-                {
-                    samples_++;
-                    std::cout << "Message: " << hello_.message() << " with index: " << hello_.index()
-                              << " RECEIVED." << std::endl;
-                }
-            }
-        }
-
-        HelloWorld hello_;
-
-        std::atomic_int samples_;
-
-    }
-    listener_;
-
-public:
-
-    HelloWorldSubscriber()
-        : participant_(nullptr)
-        , subscriber_(nullptr)
-        , topic_(nullptr)
-        , reader_(nullptr)
-        , type_(new HelloWorldPubSubType())
-    {
+    void on_subscription_matched(
+        DataReader*, const SubscriptionMatchedStatus& info) override {
+      if (info.current_count_change == 1) {
+        std::cout << "Subscriber matched." << std::endl;
+      } else if (info.current_count_change == -1) {
+        std::cout << "Subscriber unmatched." << std::endl;
+      } else {
+        std::cout << info.current_count_change
+                  << " is not a valid value for SubscriptionMatchedStatus "
+                     "current count change"
+                  << std::endl;
+      }
     }
 
-    virtual ~HelloWorldSubscriber()
-    {
-        if (reader_ != nullptr)
-        {
-            subscriber_->delete_datareader(reader_);
+    void on_data_available(DataReader* reader) override {
+      SampleInfo info;
+      if (reader->take_next_sample(&hello_, &info) ==
+          eprosima::fastdds::dds::RETCODE_OK) {
+        if (info.valid_data) {
+          samples_++;
+          std::cout << "Message: " << hello_.message()
+                    << " with index: " << hello_.index() << " RECEIVED."
+                    << std::endl;
         }
-        if (topic_ != nullptr)
-        {
-            participant_->delete_topic(topic_);
-        }
-        if (subscriber_ != nullptr)
-        {
-            participant_->delete_subscriber(subscriber_);
-        }
-        DomainParticipantFactory::get_instance()->delete_participant(participant_);
+      }
     }
 
-    //!Initialize the subscriber
-    bool init()
-    {
-        DomainParticipantQos participantQos;
-        participantQos.name("Participant_subscriber");
-        participant_ = DomainParticipantFactory::get_instance()->create_participant(0, participantQos);
+    HelloWorld hello_;
 
-        if (participant_ == nullptr)
-        {
-            return false;
-        }
+    std::atomic_int samples_;
 
-        // Register the Type
-        type_.register_type(participant_);
+  } listener_;
 
-        // Create the subscriptions Topic
-        topic_ = participant_->create_topic("HelloWorldTopic", "HelloWorld", TOPIC_QOS_DEFAULT);
+ public:
+  HelloWorldSubscriber()
+      : participant_(nullptr),
+        subscriber_(nullptr),
+        topic_(nullptr),
+        reader_(nullptr),
+        type_(new HelloWorldPubSubType()) {}
 
-        if (topic_ == nullptr)
-        {
-            return false;
-        }
+  virtual ~HelloWorldSubscriber() {
+    if (reader_ != nullptr) {
+      subscriber_->delete_datareader(reader_);
+    }
+    if (topic_ != nullptr) {
+      participant_->delete_topic(topic_);
+    }
+    if (subscriber_ != nullptr) {
+      participant_->delete_subscriber(subscriber_);
+    }
+    DomainParticipantFactory::get_instance()->delete_participant(participant_);
+  }
 
-        // Create the Subscriber
-        subscriber_ = participant_->create_subscriber(SUBSCRIBER_QOS_DEFAULT, nullptr);
+  //! Initialize the subscriber
+  bool init() {
+    DomainParticipantQos participantQos;
+    participantQos.name("Participant_subscriber");
+    participant_ = DomainParticipantFactory::get_instance()->create_participant(
+        0, participantQos);
 
-        if (subscriber_ == nullptr)
-        {
-            return false;
-        }
-
-        // Create the DataReader
-        reader_ = subscriber_->create_datareader(topic_, DATAREADER_QOS_DEFAULT, &listener_);
-
-        if (reader_ == nullptr)
-        {
-            return false;
-        }
-
-        return true;
+    if (participant_ == nullptr) {
+      return false;
     }
 
-    //!Run the Subscriber
-    void run(
-            uint32_t samples)
-    {
-        while (listener_.samples_ < samples)
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        }
+    // Register the Type
+    type_.register_type(participant_);
+
+    // Create the subscriptions Topic
+    topic_ = participant_->create_topic("HelloWorldTopic", "HelloWorld",
+                                        TOPIC_QOS_DEFAULT);
+
+    if (topic_ == nullptr) {
+      return false;
     }
 
+    // Create the Subscriber
+    subscriber_ =
+        participant_->create_subscriber(SUBSCRIBER_QOS_DEFAULT, nullptr);
+
+    if (subscriber_ == nullptr) {
+      return false;
+    }
+
+    // Create the DataReader
+    reader_ = subscriber_->create_datareader(topic_, DATAREADER_QOS_DEFAULT,
+                                             &listener_);
+
+    if (reader_ == nullptr) {
+      return false;
+    }
+
+    return true;
+  }
+
+  //! Run the Subscriber
+  void run(uint32_t samples) {
+    while (listener_.samples_ < samples) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  }
 };
 
-int main(
-        int argc,
-        char** argv)
-{
-    std::cout << "Starting subscriber." << std::endl;
-    uint32_t samples = 10;
+int main(int argc, char** argv) {
+  std::cout << "Starting subscriber." << std::endl;
+  uint32_t samples = 10;
 
-    HelloWorldSubscriber* mysub = new HelloWorldSubscriber();
-    if (mysub->init())
-    {
-        mysub->run(samples);
-    }
+  HelloWorldSubscriber* mysub = new HelloWorldSubscriber();
+  if (mysub->init()) {
+    mysub->run(samples);
+  }
 
-    delete mysub;
-    return 0;
+  delete mysub;
+  return 0;
 }
